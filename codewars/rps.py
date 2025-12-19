90845e9f303cb5e2e475a23ff6d5909f37bed04c
chikaramike <8148271+chikaramike@users.noreply.github.com>
2025-12-19 09:45:43 +0900

    Make logic work

--- codewars/rps.py
+++ codewars/rps.py
@@ -1,11 +1,26 @@
 def rps(p1, p2):
-    if p1 == "rock" and p2 == "scissors":
-        print("P1 Won")
-    elif p1 == "rock" and p2 == "paper":
-        print("P1 Lost")
-    elif p1 == p2:
-        print("Draw")
+    if p1 == p2:
+        print("draw")
+    elif p1 == "rock":
+        if p2 == "scissors":
+            print("P1 Won")
+        elif p2 == "paper":
+            print("P2 Won")
+    elif p1 == "paper":
+        if p2 == "scissors":
+            print("P2 Won")
+        elif p2 == "rock":
+            print("P1 Won")
+    elif p1 == "scissors":
+        if p2 == "rock":
+            print("P2 Won")
+        elif p2 == "paper":
+            print("P1 Won")
+
+
 
 rps("rock", "scissors")
 rps("rock", "paper")
 rps("rock", "rock")
+rps("paper", "rock")
+rps("scissors", "rock")
