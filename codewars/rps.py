5e22f7ece1053cd28590e962348fa27434f87ed2
chikaramike <8148271+chikaramike@users.noreply.github.com>
2025-12-19 09:19:26 +0900

    Initial set-up

--- codewars/rps.py
+++ codewars/rps.py
@@ -1,5 +1,11 @@
 def rps(p1, p2):
-    print(p1)
-    print(p2)
+    if p1 == "rock" and p2 == "scissors":
+        print("P1 Won")
+    elif p1 == "rock" and p2 == "paper":
+        print("P1 Lost")
+    elif p1 == p2:
+        print("Draw")
 
 rps("rock", "scissors")
+rps("rock", "paper")
+rps("rock", "rock")
